e3f1bf3de6aab49e48b5361547bebf765dbb464f
chris cook <chriscook13@hotmail.co.uk>
2023-02-25 17:08:13 +0000

    added selectable options for terminal command intern/engineer

--- starter/index.js
+++ starter/index.js
@@ -13,10 +13,110 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-//manager extends employee function
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "employeeId",
+            message: "What is your Employee ID?"
+        },
+        {
+            type: "input",
+            name: "emailAddress",
+            message: "What is your Email Address?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your Office Number?"
+        },
+        {
+            type: "list",
+            name: "teamOptions",
+            message: "Do you want to add a team member",
+            choices: ['Engineer', 'Intern', 'Finish team']
+        }
+    ])
 
-//engineer extends employee function
 
-//interns extend employee function
 
+    .then((answers) => {
+
+
+        if (answers.teamOptions == "Engineer") {
+            inquirer
+                .prompt([
+
+                    {
+                        type: "input",
+                        name: "engineerName",
+                        message: "Enter engineer's name"
+                    },
+                    {
+                        type: "input",
+                        name: "engineerId",
+                        message: "Enter engineer's ID"
+                    },
+                    {
+                        type: "input",
+                        name: "engineerEmail",
+                        message: "Enter engineer's email address"
+                    },
+                    {
+                        type: "input",
+                        name: "engineerGithub",
+                        message: "Enter the engineers GitHub"
+                    },
+                    {
+                        type: "list",
+                        name: "teamOptions",
+                        message: "Do you want to add a team member",
+                        choices: ['Engineer', 'Intern', 'Finish team']
+                    }
+                ])
+        } else if (answers.teamOptions == "Intern") {
+            inquirer
+                .prompt([
+
+                    {
+                        type: "input",
+                        name: "internName",
+                        message: "Enter intern's name?"
+                    },
+                    {
+                        type: "input",
+                        name: "internId",
+                        message: "Enter intern's ID?"
+                    },
+                    {
+                        type: "input",
+                        name: "internEmail",
+                        message: "Enter intern's email address?"
+                    },
+                    {
+                        type: "input",
+                        name: "internSchool",
+                        message: "Enter the name of the interns school:"
+                    },
+                    {
+                        type: "list",
+                        name: "teamOptions",
+                        message: "Do you want to add a team member",
+                        choices: ['Engineer', 'Intern', 'Finish team']
+                    }
+                ])
+        } else {
+            //need to render
+            render
+        }
+
+        /*fs.writeFile(outputPath)
+        */
+
+    })
 
