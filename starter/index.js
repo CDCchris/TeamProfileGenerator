fd453b2acf1f939c0c4381db62964935d98d9385
chris cook <chriscook13@hotmail.co.uk>
2023-03-02 21:46:57 +0000

    changed index.js to indivudal functions, added function to write html

--- starter/index.js
+++ starter/index.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const teamInfo = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,110 +14,147 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your name?"
-        },
-        {
-            type: "input",
-            name: "employeeId",
-            message: "What is your Employee ID?"
-        },
-        {
-            type: "input",
-            name: "emailAddress",
-            message: "What is your Email Address?"
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "What is your Office Number?"
-        },
-        {
-            type: "list",
-            name: "teamOptions",
-            message: "Do you want to add a team member",
-            choices: ['Engineer', 'Intern', 'Finish team']
-        }
-    ])
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the managers name?"
+            },
+            {
+                type: "input",
+                name: "employeeId",
+                message: "What the Manager's ID?"
+            },
+            {
+                type: "input",
+                name: "emailAddress",
+                message: "What the Managers Email Address?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What the Managers Office Number?"
+            },
+        ])
+        .then((answers) => {
+            // what to do with the responses from the user???
+            const managerInfo = new Manager(answers.name, answers.employeId, answers.emailAddress, answers.officeNumber);
+            teamInfo.push(managerInfo)
+
+            // need to now ask if the user the menu
+            menu()
+        })
+}
+
+function finishTeam(){
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamInfo), "utf-8")
+}
 
 
 
-    .then((answers) => {
+function menu() {
+    inquirer
+        .prompt(
+            {
+                type: "list",
+                name: "teamOptions",
+                message: "Do you want to add a team member",
+                choices: ['Engineer', 'Intern', 'Finish team']
+            }
+        )
+        .then((answers) => {
+
+            switch (answers.teamOptions) {
+                case 'Engineer':
+                    engineerPrompt()
+                    break;
+                case 'Intern':
+                    internPrompt()
+                    break;
+                case 'Finish team':
+                    finishTeam()
+                    break;
+            }
+        })
+}
 
 
-        if (answers.teamOptions == "Engineer") {
-            inquirer
-                .prompt([
+function engineerPrompt() {
+    inquirer
+        .prompt([
 
-                    {
-                        type: "input",
-                        name: "engineerName",
-                        message: "Enter engineer's name"
-                    },
-                    {
-                        type: "input",
-                        name: "engineerId",
-                        message: "Enter engineer's ID"
-                    },
-                    {
-                        type: "input",
-                        name: "engineerEmail",
-                        message: "Enter engineer's email address"
-                    },
-                    {
-                        type: "input",
-                        name: "engineerGithub",
-                        message: "Enter the engineers GitHub"
-                    },
-                    {
-                        type: "list",
-                        name: "teamOptions",
-                        message: "Do you want to add a team member",
-                        choices: ['Engineer', 'Intern', 'Finish team']
-                    }
-                ])
-        } else if (answers.teamOptions == "Intern") {
-            inquirer
-                .prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "Enter engineer's name"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "Enter engineer's ID"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "Enter engineer's email address"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "Enter the engineers GitHub"
+            },
+        ])
+        .then((answers) => {
+            const engineerInfo = new Engineer(answers.Engineername, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamInfo.push(engineerInfo)
 
-                    {
-                        type: "input",
-                        name: "internName",
-                        message: "Enter intern's name?"
-                    },
-                    {
-                        type: "input",
-                        name: "internId",
-                        message: "Enter intern's ID?"
-                    },
-                    {
-                        type: "input",
-                        name: "internEmail",
-                        message: "Enter intern's email address?"
-                    },
-                    {
-                        type: "input",
-                        name: "internSchool",
-                        message: "Enter the name of the interns school:"
-                    },
-                    {
-                        type: "list",
-                        name: "teamOptions",
-                        message: "Do you want to add a team member",
-                        choices: ['Engineer', 'Intern', 'Finish team']
-                    }
-                ])
-        } else {
-            //need to render
-            render
-        }
+            menu()
+        })
+}
 
-        /*fs.writeFile(outputPath)
-        */
+function internPrompt() {
+    inquirer
+        .prompt([
 
-    })
+            {
+                type: "input",
+                name: "internName",
+                message: "Enter intern's name?"
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "Enter intern's ID?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "Enter intern's email address?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "Enter the name of the interns school:"
+            },
 
+        ])
+        .then((answers) => {
+
+            const internInfo = new Engineer(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamInfo.push(internInfo)
+
+            menu()
+        })
+
+}
+
+// build the function that will write the html
+
+
+
+init()
